223c95b4a301cc1919d17279c58dd8aaa486a608
VadimKh <m8740012@gmail.com>
2016-05-08 19:20:28 +0300

    Add trell authorization

--- app/scripts/newtab.js
+++ app/scripts/newtab.js
@@ -1,3 +1,34 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Popup');
+$(function(){
+
+  $.ajax({
+      url: 'http://image-a-day.herokuapp.com/image',
+      type: 'GET',
+      crossDomain: true,
+      dataType: 'json',
+      success: function(data) {
+        document.getElementById("bg").style.backgroundImage = 'url(' + data.imageUrl + ')';
+      },
+      error: function() {
+        document.getElementById("bg").style.backgroundImage = 'url(../images/background.jpg)';
+      }
+  });
+
+  $("#test").on("click", function(){
+    Trello.setKey('145ab72d8820e386341eda162214b61a');
+    Trello.authorize({
+        name: "Trello Helper Extension",
+        type: "redirect",
+        expiration: "never",
+        interactive: true,
+        scope: {read: true, write: false},
+        success: function () {
+            // Can't do nothing, we've left the page
+        },
+        error: function () {
+            alert("Failed to authorize with Trello. Please tell me about your problem at bruno@skvorc.me.")
+        }
+    });
+  });
+});
