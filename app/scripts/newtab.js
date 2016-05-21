7ca749d54feae820d34c25807953dd29d8a31f46
VadimKh <m8740012@gmail.com>
2016-05-21 08:36:25 +0300

    Add trello authorization page

--- app/scripts/newtab.js
+++ app/scripts/newtab.js
@@ -1,7 +1,6 @@
 'use strict';
 
 $(function(){
-
   $.ajax({
       url: 'http://image-a-day.herokuapp.com/image',
       type: 'GET',
@@ -14,9 +13,13 @@ $(function(){
         document.getElementById("bg").style.backgroundImage = 'url(../images/background.jpg)';
       }
   });
+});
 
-  $("#test").on("click", function(){
-    Trello.setKey('145ab72d8820e386341eda162214b61a');
+$(function(){
+  Trello.setKey('145ab72d8820e386341eda162214b61a');
+  var tokenHashRegEx = /token/;
+
+  if(localStorage.trello_token || tokenHashRegEx.test(window.location.hash)) {
     Trello.authorize({
         name: "Trello Helper Extension",
         type: "redirect",
@@ -24,11 +27,29 @@ $(function(){
         interactive: true,
         scope: {read: true, write: false},
         success: function () {
-            // Can't do nothing, we've left the page
         },
         error: function () {
-            alert("Failed to authorize with Trello. Please tell me about your problem at bruno@skvorc.me.")
+            alert("Failed to authorize with Trello. Please tell me about your problem at khoroshiltsev.vadim@gmail.com.")
         }
     });
-  });
-});
+  } else {
+    var dialog = document.getElementById('needAuthorization');
+    dialog.showModal();
+    $('body').addClass('opened-dialog');
+    document.getElementById('authorize').addEventListener('click', function(){
+      Trello.authorize({
+          name: "Trello Helper Extension",
+          type: "redirect",
+          expiration: "never",
+          interactive: true,
+          scope: {read: true, write: false},
+          success: function () {
+          },
+          error: function () {
+              alert("Failed to authorize with Trello. Please tell me about your problem at khoroshiltsev.vadim@gmail.com.")
+          }
+      });
+    });
+  }
+
+})
